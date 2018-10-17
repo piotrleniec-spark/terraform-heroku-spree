66023fa00447830a75cd8594213b7dd9e963091b
Piotr Leniec <leniec.piotr@gmail.com>
2018-10-17 21:43:00 +0200

    add heroku app provisioning

--- main.tf
+++ main.tf
@@ -15,9 +15,27 @@ resource "heroku_app" "spree" {
   config_vars = {
     RAILS_ENV = "production"
   }
+
+  provisioner "local-exec" {
+    command = <<-SHELL
+      cd /tmp
+      git clone https://github.com/piotrleniec/spree-app.git
+      cd spree-app
+      git remote add heroku ${heroku_app.spree.git_url}
+      git push heroku master
+      cd ..
+      rm -rf spree-app
+    SHELL
+  }
 }
 
 resource "heroku_addon" "database" {
   app = "${heroku_app.spree.name}"
   plan = "heroku-postgresql:hobby-dev"
+
+  provisioner "local-exec" {
+    command = <<-SHELL
+      heroku run 'AUTO_ACCEPT=1 bundle exec rails db:schema:load db:seed spree_sample:load' -a ${heroku_app.spree.name}
+    SHELL
+  }
 }
